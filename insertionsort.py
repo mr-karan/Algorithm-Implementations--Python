d8ad6d45d9ab7f6c0d5aa607adcb394421d9b4c5
Karan Sharma <karansharma1295@gmail.com>
2015-09-12 16:17:49 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,7 +1,9 @@
-a=[] // fill it up with numbers
-
+a=[1,4,3,5,6,2]
 for i in range(1,len(a)):
-  j=i
-  while(j>0 and a[j-1]>a[j]):
-    a[j],a[j-1]=a[j-1],a[j]
-    
+	j=i-1
+	key=a[i]
+	while(j>=0) and (a[j]>key):
+		a[j+1]=a[j]
+		j=j-1
+	a[j+1]=key
+	print (a)
